b2307e44bdbda22a98c8e0f94b1bdf70b97b18f7
Muneeb <muneebshahid5@gmail.com>
2025-07-30 14:04:58 +0200

    Add logging and complete MultiHeadAttention implementation with split_heads method

--- papers/2017-attention-is-all-you-need/transformer.py
+++ papers/2017-attention-is-all-you-need/transformer.py
@@ -1,5 +1,9 @@
+import logging
+
 import torch
 import torch.nn as nn
+
+logger = logging.getLogger()
 
 
 class MultiHeadAttention(nn.Module):
@@ -15,5 +19,35 @@ class MultiHeadAttention(nn.Module):
         self.W_v = nn.Linear(d_model, d_model)  # Value transformation
         self.W_o = nn.Linear(d_model, d_model)  # Output transformation
 
-    def forward(self, x, mask=None):
-        B, T, C = x.size()
+    def split_heads(self, batch_size, seq_len, tensor) -> torch.tensor:
+        return tensor.view(
+            batch_size, seq_len, self.num_heads, self.head_dim
+        ).transpose(1, 2)
+
+    def forward(self, x):
+        batch_size, seq_len, _ = x.shape
+        q = self.W_q(x)  # q.shape (batch_size, seq_len, d_model)
+        k = self.W_k(x)  # k.shape (batch_size, seq_len, d_model)
+        v = self.W_v(x)  # v.shape (batch_size, seq_len, d_model)
+
+        q = self.split_heads(
+            batch_size, seq_len, q
+        )  # q.shape (batch_size, num_heads, seq_len, head_dim)
+        k = self.split_heads(
+            batch_size, seq_len, k
+        )  # k.shape (batch_size, num_heads, seq_len, head_dim)
+        v = self.split_heads(
+            batch_size, seq_len, v
+        )  # v.shape (batch_size, num_heads, seq_len, head_dim)
+
+        qk = q @ k.transpose(-2, -1)
+
+
+if __name__ == "__main__":
+    logger.info("Running Transformer")
+    d_model = 6
+    num_heads = 2
+    seq_len = 4
+    mh = MultiHeadAttention(d_model, num_heads)
+    x = torch.randn(2, seq_len, d_model)
+    mh.forward(x)
