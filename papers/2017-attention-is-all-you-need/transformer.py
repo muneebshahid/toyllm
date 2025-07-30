f03c3f701bc37c521c349cf4c08380d37e9adcc4
Muneeb <muneebshahid5@gmail.com>
2025-07-30 21:30:17 +0200

    Add PositionalEncoding and TransformerEncoder classes to implement positional encodings and initial embedding setup

--- papers/2017-attention-is-all-you-need/transformer.py
+++ papers/2017-attention-is-all-you-need/transformer.py
@@ -75,6 +75,58 @@ class MultiHeadAttention(nn.Module):
         return y
 
 
+class PositionalEncoding(nn.Module):
+    """Implements the positional encodings as described in the paper
+    "Attention is All You Need" (Vaswani et al., 2017).
+        p_i = sin(i / 10000^(2j/d_model)) for j even
+        p_i = cos(i / 10000^(2j/d_model)) for j odd
+    """
+
+    def __init__(self, d_model: int, max_len: int = 5000):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(torch.log(torch.tensor(10000.0)) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        return x + self.pe[:, : x.size(1)]
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(
+        self,
+        vocab_size: int,
+        d_model: int,
+        num_heads: int,
+        d_ff: int,
+        num_layers: int,
+        max_len=512,
+    ):
+        self.embed = nn.Embedding(vocab_size, d_model)
+
+    # def
+
+
+class Transformer(nn.module):
+    def __init__(
+        self,
+        src_vocab_size: int,
+        tgt_vocab_size: int,
+        d_model: int,
+        num_heads: int,
+        num_layers: int,
+        d_ff: int,
+    ):
+        super().__init__()
+        self.encoder_embedding = nn.Embedding
+
+
 if __name__ == "__main__":
     logger.info("Running Transformer")
     d_model = 6
